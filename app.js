357c62e1c20ccfb86ef998db62290d4638e7ad6b
anaviana3-blip <ana.viana3@estudante.ifms.edu.br>
2026-08-30 00:55:03 +0000

    core: criei uma rota GET para buscar produtos por ID, retornando 404 caso não exista.

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
-import express from 'express';
+import express from 'express'; 
+ 
+const app = express(); 
+const PORT = 3000; 
+ 
+const usuarios = [ 
+    {id: 1, nome: 'Pedro'}, 
+    {id: 2, nome: 'Sarah'} 
+]; 
 
-const app = express();
-const PORT = 3000;
-
-const usuarios = [
-    {id: 1, nome: 'Pedro'},
-    {id: 2, nome: 'Sarah'}
+const produtos = [
+    {id: 1, nome: 'Notebook'},
+    {id: 2, nome: 'Celular'},
+    {id: 3, nome: 'Camiseta'}
 ];
+ 
+app.get('/usuarios', (req, res) => { 
+    res.json(usuarios); 
+}); 
+ 
+app.post('/usuario', (req, res) => { 
+    const novoUsuario = { 
+        id: usuarios.length + 1, 
+        nome: 'João' 
+    }; 
+    usuarios.push(novoUsuario); 
+    res.status(201).json(novoUsuario); 
+     
+}); 
 
-app.get('/usuarios', (req, res) => {
-    res.json(usuarios);
+app.get('/produtos/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const produto = produtos.find(p => p.id === id);
+
+    if (!produto) {
+        return res.status(404).json({ mensagem: 'Produto não encontrado' });
+    }
+
+    res.json(produto);
 });
-
-app.post('/usuario', (req, res) => {
-    const novoUsuario = {
-        id: usuarios.length + 1,
-        nome: 'João'
-    };
-    usuarios.push(novoUsuario);
-    res.status(201).json(novoUsuario);
-    
-});
-
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+ 
+app.listen(PORT, () => { 
+    console.log(`Servidor rodando em http://localhost:${PORT}`); 
 });
